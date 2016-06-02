f6517b84aaf32b6d44a541ab3c06768bf46a602b
Ogbara Godwin <godwin.onisofien@andela.com>
2016-06-02 16:28:18 +0100

    feat(router): add mapping of routes
    [Finishes #120522471]

--- lib/roh.rb
+++ lib/roh.rb
@@ -1,5 +1,27 @@
+require "pry"
 require "roh/version"
+require "roh/routing/router.rb"
 
 module Roh
-  # Your code goes here...
+  class Application
+    attr_accessor :routes
+
+    def initialize
+      @routes ||= Routing::Router.new
+    end
+
+    def call(env)
+      # hide_favicon
+      # get_rack_app(env)
+      [ 200, {}, ["Hello world"]]
+    end
+
+    private
+
+    def hide_favicon
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {}, []]
+      end
+    end
+  end
 end
---
+++ lib/roh/routing/router.rb
@@ -0,0 +1,42 @@
+module Roh
+  module Routing
+    class Router
+      attr_accessor :endpoints
+
+      def endpoints
+        @endpoints ||= Hash.new { |hash, key| hash[key] = [] }
+      end
+
+      def draw(&block)
+        instance_eval(&block)
+      end
+
+      [:get, :post, :put, :patch, :delete].each do |method_name|
+        define_method(method_name) do |url, *options|
+          route_data = {
+            path: url,
+            pattern: regexp_pattern(url)
+          }
+          endpoints[method_name] << route_data
+        end
+      end
+
+      def regexp_pattern(url_elements)
+        # TODO: Refactor this
+        url_elements = url_elements.split("/")
+        placeholder = []
+
+        url_elements.each do |element|
+          if element.start_with?(":")
+            placeholder << "(?<id>\\d+)"
+          else
+            placeholder << element
+          end
+        end
+
+        Regexp.new(placeholder.join("/"))
+      end
+
+    end
+  end
+end
