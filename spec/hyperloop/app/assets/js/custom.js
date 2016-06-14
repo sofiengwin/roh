ae6a5a68ec41b1ea1832b0f39d4b40ae904f1bb8
Ogbara Godwin <godwin.onisofien@andela.com>
2016-06-14 20:57:20 +0100

    feat(views): add basic style to views

--- spec/hyperloop/app/assets/js/custom.js
+++ spec/hyperloop/app/assets/js/custom.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
   //Hide Search field
   $( "#exTab3" ).click(function(event) {
-    event.preventDefault();
+    // console.log("hello world");
+    // event.preventDefault();
   });
 });
